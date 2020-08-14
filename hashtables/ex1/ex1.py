1ac0a571fefd2fbd5561540d4c2fa3f664bc76d1
Barbara Mayfield <bmayfield1991@gmail.com>
2020-08-14 18:13:00 -0500

    work: ex1.py done

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -2,6 +2,14 @@ def get_indices_of_item_weights(weights, length, limit):
     """
     YOUR CODE HERE
     """
-    # Your code here
+    weight_dict = {}
 
-    return None
+    for index in range(length):
+        val = weights[index]
+        target = limit - val
+
+        if target not in weight_dict:
+            weight_dict[val] = index
+        else:
+            target_index = weight_dict[target]
+            return (index, target_index)
