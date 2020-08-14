4bbe4e87dba0fef5cd8cb411e70549abe8cbbada
Barbara Mayfield <bmayfield1991@gmail.com>
2020-08-14 18:25:22 -0500

    work: ex2.py done

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -1,14 +1,24 @@
 #  Hint:  You may not need all of these.  Remove the unused functions.
 class Ticket:
     def __init__(self, source, destination):
-        self.source = source
-        self.destination = destination
+        self.source = source  # starting airport
+        self.destination = destination  # next airport
 
 
 def reconstruct_trip(tickets, length):
     """
     YOUR CODE HERE
     """
-    # Your code here
+    Ticket = {}
+    route = []
+
+    for ticket in tickets:
+        Ticket[ticket.source] = ticket.destination
+
+    for _ in range(length):
+        if not route:
+            route.append(Ticket['NONE'])
+        else:
+            route.append(Ticket[route[-1]])
 
     return route
