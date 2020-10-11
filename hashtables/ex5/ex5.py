d95284cd4ad7ef426eb82b7b5c59c79e4a2067ef
Barbara Mayfield <bmayfield1991@gmail.com>
2020-10-11 17:57:44 -0500

    work: exp5.py done

--- hashtables/ex5/ex5.py
+++ hashtables/ex5/ex5.py
@@ -1,5 +1,29 @@
 # Your code here
 
+# U P E R
+
+# U - Understand
+# Input:
+# We are given:
+#   - list of full paths to files
+#   - list of filenames to query
+# Output:
+# We need to return:
+#   - List of strings of full paths that match the filename.
+
+# Plan
+# Dict for file names
+# List of results
+# For the path in our files
+# We will want to split the path at it's /'s
+# Set file_name to the end of the split path
+# If the file name isn't in the dict,
+# Set the default file name to the file name and path
+# Otherwise, append the path to the file name's index.
+
+# As for queries append the path if the query is in the dict
+
+# Execute
 
 
 def finder(files, queries):
@@ -7,7 +31,21 @@ def finder(files, queries):
     YOUR CODE HERE
     """
     # Your code here
+    file_names = {}
+    result = []
 
+    for path in files:
+        split_path = path.split("/")
+        file_name = split_path[-1]
+        if file_name not in file_names:
+            file_names.setdefault(file_name, [path])
+        else:
+            file_names[file_name].append(path)
+
+    for query in queries:
+        if query in file_names:
+            for path in file_names[query]:
+                result.append(path)
     return result
 
 
@@ -23,3 +61,6 @@ if __name__ == "__main__":
         "baz"
     ]
     print(finder(files, queries))
+
+
+# Reflect
