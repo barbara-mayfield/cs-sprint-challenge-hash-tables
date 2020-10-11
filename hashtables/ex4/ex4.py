067affa93814938a4cd3c13e4e9efcae50f1a890
Barbara Mayfield <bmayfield1991@gmail.com>
2020-10-11 17:50:32 -0500

    work: ex4.py done

--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -1,11 +1,31 @@
-def has_negatives(a):
+# U P E R
+
+# U - Understand
+# We need to solve this problem with a hash table.
+
+# Input: An unordered list of positive and negative integers.
+# Output: Positive numbers that have corresponding negative numbers.
+
+# P - Plan
+# E - Execute
+
+def has_negatives(integers):
     """
     YOUR CODE HERE
     """
-    # Your code here
+    table = {}
+    result = []
 
+    for integer in integers:
+        if integer != 0:
+            table[integer] = 1
+            if -integer in table:
+                # abs returns absolute value of a num
+                result.append(abs(integer))
     return result
 
 
 if __name__ == "__main__":
     print(has_negatives([-1, -2, 1, 2, 3, 4, -4]))
+
+# R - Reflect
